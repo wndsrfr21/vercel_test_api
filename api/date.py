0916d4a31d357e6273f4a2605e0821738a305b11
Alex Donn <wndsrfr21@gmail.com>
2021-06-14 00:14:31 -0400

    Update date.py

--- api/date.py
+++ api/date.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
 
@@ -8,4 +9,11 @@ class handler(BaseHTTPRequestHandler):
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    s = self.path
+    dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+            if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
+    self.wfile.write(message.encode())
     return
